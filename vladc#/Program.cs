32ee790471adc9ff7bbf9ef87ab23eaea0cbfd54
vladarella <vladarella158@gmail.com>
2026-06-23 16:07:46 +0300

    Дописал стишок

--- vladc#/Program.cs
+++ vladc#/Program.cs
@@ -4,8 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!!!");
-            Console.WriteLine("vlad");
+            Console.WriteLine("Я утром сел писать программу,\nИ когда написал не мало.");
+            Console.WriteLine("Вдруг компилятор мне сказал:\n\"Ошибка здесь\" - и указал.\nЯ долго баг потом искал,");
+            Console.WriteLine("Но все же код свой дописал.");
+            Console.WriteLine("Теперь программа без проблем\nРаботает на радость всем.");
         }
     }
 }
