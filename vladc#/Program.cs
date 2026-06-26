e1489916a77de4d1d41c2ced1052181ff301eb26
vladarella <vladarella158@gmail.com>
2026-06-26 18:10:55 +0300

    вернули обратно

--- vladc#/Program.cs
+++ vladc#/Program.cs
@@ -12,8 +12,7 @@
             //зделали парочку переменных
             Console.WriteLine(text + "ему " + number + ".\nОн работает " + number2 + " лет\nЕго любимый символ - " + symbol);
             Console.WriteLine("Есть ли у него питомец? - " + result);
-            string text2 = Console.ReadLine();
-            Console.WriteLine("Привет! " + text2);
+            
             
 
         }
