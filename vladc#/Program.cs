deaade33933372500475cc43ecdc55ce4fa3d80a
vladarella <vladarella158@gmail.com>
2026-06-27 22:47:36 +0300

    Хорошо поработали над вводом

--- vladc#/Program.cs
+++ vladc#/Program.cs
@@ -4,17 +4,34 @@
     {
         static void Main(string[] args)
         {
-            string text = "Вася - Учиеть програмироваия ";
-            int number = 30;
-            Double number2 = 7.6;
-            char symbol = '@';
-             bool result = true;
+            //string text = "Вася - Учиеть програмироваия ";
+            //int number = 30;
+            //Double number2 = 7.6;
+            //char symbol = '@';
+            //bool result = true;
             //зделали парочку переменных
-             Console.WriteLine(text + "ему " + number + ".\nОн работает " + number2 + " лет\nЕго любимый символ - " + symbol);
-            Console.WriteLine("Есть ли у него питомец? - " + result);
-            string text2 = Console.ReadLine();
-            Console.WriteLine("Привет !" + text2);
-                                                                                                 
+            //Console.WriteLine(text + "ему " + number + ".\nОн работает " + number2 + " лет\nЕго любимый символ - " + symbol);
+            //Console.WriteLine("Есть ли у него питомец? - " + result);
+            Console.Write("Введите ваше имя. ");
+            string name = Console.ReadLine();
+
+            Console.Write("Сколько вам лет?");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("На ваше усмотрение какой самый красивый символ?");
+            char symbol = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Сегодня хорошая погода?");
+            bool result = Convert.ToBoolean(Console.ReadLine());
+
+            Console.Write("Который час?");
+            double number = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Ваше имя - {name}");
+            Console.WriteLine($"Вам {age} лет");
+            Console.WriteLine($"Ваш любимый символ - {symbol}");
+            Console.WriteLine($"{result}");
+            Console.WriteLine($"{number}");
         }
     }
 }
