8046d3d4c75f1878280cd1f6f9be15a6a78f126c
vladarella <vladarella158@gmail.com>
2026-07-03 15:22:57 +0300

    Игровой автомат

--- vladc#/Program.cs
+++ vladc#/Program.cs
@@ -1,45 +1,90 @@
-﻿namespace vladc_
+﻿using System.ComponentModel.Design;
+
+namespace vladc_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Первая задача
-            //Console.Write("Введите первое число: ");
-            //int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите ставку");
+            int ставки = int.Parse(Console.ReadLine());
 
-            //Console.Write("Введите второе число: ");
-            //int y = int.Parse(Console.ReadLine());
+            if (ставки < 5 || ставки > 100 || ставки % 5 != 0)
+            {
+                Console.WriteLine("Ошибка! некорректная ставка");
+                return;
+            }
 
-            //Console.WriteLine($"Решим пример  - (a + b):2 Подставляем первое и второе числа и получаеться = {(double)(x + y)/2}");
+            Random чиселки = new Random();
+            int A = чиселки.Next(1, 10);
+            int B = чиселки.Next(1, 10);
+            int C = чиселки.Next(1, 10);
 
-            //Вторая задача
-            //Console.Write("Введите первое число: ");
-            //int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Вращаем барабан");
+            Console.WriteLine($"{A} {B} {C}");
 
-            //Console.Write("Введите второе число: ");
-            //int B = int.Parse(Console.ReadLine());
+            double коэффициент = 0;
 
-            //Console.Write("Введите третье число: ");
-            //int C = int.Parse(Console.ReadLine());
+            // три подряд одинаковых числа
+            if (A == B && B == C)
+            {
+                if (A == 1) коэффициент = 10 * 1.5;
+                else if (A == 2) коэффициент = 20 * 1.5;
+                else if (A == 3) коэффициент = 30 * 1.5;
+                else if (A == 4) коэффициент = 40 * 1.5;
+                else if (A == 5) коэффициент = 50 * 1.5;
+                else if (A == 6) коэффициент = 60 * 1.5;
+                else if (A == 7)
+                {
+                    коэффициент = 150 * 1.5;
+                    Console.WriteLine("Вы сорвали ДЖЕКПОТ!");
+                }
+                else if (A == 5) коэффициент = 80 * 1.5;
+                else if (A == 6) коэффициент = 90 * 1.5;
+            }
 
-            //Console.WriteLine($"Решим пример - (a*b):3 + c Подставляем первое второе и третье числа и получаеться = {(double)(A*B)/3 + C}");
+            //два полряд одинаковых числа
+            else if (A == B && B != C)
+            {
+                if (A == 1) коэффициент = 1 * 1.25;
+                else if (A == 2) коэффициент = 2 * 1.25;
+                else if (A == 3) коэффициент = 3 * 1.25;
+                else if (A == 4) коэффициент = 4 * 1.25;
+                else if (A == 5) коэффициент = 5 * 1.25;
+                else if (A == 6) коэффициент = 6 * 1.25;
+                if (A == 7)
+                {
+                    коэффициент = 15 * 1.25;
+                    Console.WriteLine("Вы сорвали Мини-ДЖЕКПОТ");
+                }
+                else if (A == 8) коэффициент = 8 * 1.25;
+                else if (A == 9) коэффициент = 9 * 1.25;
+            }
 
-            //Третья задача
-            Console.Write("Введите первое число: ");
-            int A = int.Parse(Console.ReadLine());
+            //хотябы одна 7
+            else if (A == 7 || B == 7 || C == 7)
+            {
+                коэффициент = 1.6;
+            }
 
-            Console.Write("Введите второе число: ");
-            int B = int.Parse(Console.ReadLine());
+            //Хотябы одна 9
+            else if (A == 9 || B == 9 || C == 9)
+            {
+                коэффициент = 1.35;
+            }
 
-            Console.Write("Введите третье число: ");
-            int C = int.Parse(Console.ReadLine());
-
-            Console.Write("Введите четвертое число: ");
-            int D = int.Parse(Console.ReadLine());
-
-            Console.WriteLine($"Решим пример - ((a*b):2 + (c*d)):3 Подставляем первое второе третье и четвертое числа и получаеться = ");
-            Console.WriteLine($"{A* (double)B/ 2 + C*D/3}");
-        }
+            if (коэффициент == 0)
+            {
+                Console.WriteLine("К сожелению,вы проиграли.");
+            }
+            else
+            {
+                double награда = ставки * коэффициент;
+                Console.WriteLine($"Поздравляе вы выиграли!");
+                Console.WriteLine("Выигрыш состовлает" + награда + "$");
+            }
+                
+            
+        }       
     }
 }
