5f338f52e6c87254817a348515a1d1118a1a974d
vladarella <vladarella158@gmail.com>
2026-07-07 16:06:36 +0300

    работа с числами

--- vladc#/Program.cs
+++ vladc#/Program.cs
@@ -6,80 +6,68 @@ namespace vladc_
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите ставку");
-            int stavky = int.Parse(Console.ReadLine());
+            //1
+            //Console.Write("Введите четырехзначное число ");
+            //int number;
+            //while (!int.TryParse(Console.ReadLine(), out number) || number < 1000 || number > 9999)
+            //{
+            //    Console.WriteLine("Ошибка! Введите четырехзначное число: ");
 
-            if (stavky < 5 || stavky > 100 || stavky % 5 != 0)
+            //}
+            //int count = 0;
+            //int digit = number % 10;
+            //if (digit % 2 == 0)
+            //    count++;
+
+            //digit = (number / 10) % 10;
+            //if (digit % 2 == 0)
+            //    count++;
+
+            //digit = (number / 100) % 10;
+            //if (digit % 2 == 0)
+            //    count++;
+
+            //digit = (number / 1000) % 10;
+            //if (digit % 2 == 0)
+            //    count++;
+
+            //Console.WriteLine($"Количество четных цыфр {count}");
+
+            //2 не знаю
+
+
+
+
+
+            //3
+            string password = "78945";
+
+            int attempts = 0;
+
+            while (attempts < 5)
             {
-                Console.WriteLine("Ошибка! некорректная ставка");
-                return;
-            }
+                Console.Write("Введите пароль: ");
+                string input = Console.ReadLine();
 
-            Random numbers = new Random();
-            int A = numbers.Next(1, 10);
-            int B = numbers.Next(1, 10);
-            int C = numbers.Next(1, 10);
-
-            Console.WriteLine("Вращаем барабан");
-            Console.WriteLine($"{A} {B} {C}");
-
-            double koef = 0;
-
-            // три подряд одинаковых числа
-            if (A == B && B == C)
-            {
-                if (A == 7)
+                if (input == password)
                 {
-                    koef = 150 * 1.5;
-                    Console.WriteLine("Вы сорвали ДЖЕКПОТ");
+                    Console.WriteLine("Добро пожаловать!");
+                    break;
                 }
                 else
                 {
-                    koef = A * 10 * 1.5;
+                    attempts++;
+
+                    if (attempts == 5)
+                    {
+                        Console.WriteLine("Ага , попытался меня взломать - не выйдет ,доступ запрешен!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный пароль. Попробуйте еще.");
+                    }
                 }
             }
-
-
-
-
-            //два полряд одинаковых числа
-            else if (A == B && B != C)
-            {
-                if (A == 7)
-                {
-                    koef = 150 * 1.5;
-                    Console.WriteLine("Вы сорвали Мини-ДЖЕКПОТ");
-                }
-                else
-                {
-                    koef = A * 1.25;
-                }
-            }
-
-            //хотябы одна 7
-            else if (A == 7 || B == 7 || C == 7)
-            {
-                koef = 1.6;
-            }
-
-            //Хотябы одна 9
-            else if (A == 9 || B == 9 || C == 9)
-            {
-                koef = 1.35;
-            }
-
-            if (koef == 0)
-            {
-                Console.WriteLine("К сожелению,вы проиграли.");
-            }
-            else
-            {
-                double win = stavky * koef;
-                Console.WriteLine($"Поздравляе вы выиграли!");
-                Console.WriteLine("Выигрыш состовлает" + win + "$");
-            }
-                
-            
-        }       
+        }
     }
 }
