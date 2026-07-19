e88be4feaf07f6ae67cf7a829ec902c08f42082d
vladarella <vladarella158@gmail.com>
2026-07-19 22:38:39 +0300

    попытка 2

--- vladc#/Program.cs
+++ vladc#/Program.cs
@@ -6,24 +6,42 @@
         {
             //int[,] numbers = new int[3, 5];
 
-            //Console.WriteLine("рандомные числа:");
-            ////строки
-            //for (int i = 0; i < 3; i++)
-            //{  //столбики
-            //    for (int j = 0; j < 5; j++)
+            //Console.WriteLine("Рандомные числа:");
+
+            ////  Делаем саму таблицу с числами
+            //for (int i = 0; i < numbers.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < numbers.GetLength(1); j++)
             //    {
             //        numbers[i, j] = new Random().Next(10, 21);
-            //        Console.Write(numbers[i, j] + "\t");
             //    }
-            //    Console.WriteLine();
             //}
 
-            //Console.Write("Введите номер строки (0-2): ");
-            //int line = Convert.ToInt32(Console.ReadLine());
+            //// проводим вывод таблицы с числами
+            //for (int i = 0; i < numbers.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < numbers.GetLength(1); j++)
+            //    {
+            //        Console.Write(numbers[i, j] + "\t");
+            //    }
+
+            //    Console.WriteLine("\n");
+            //}
+
+
+
+            //int line;
+            //Console.Write(" Введите номер строки (0-2): ");
+            //while (!int.TryParse(Console.ReadLine(), out line) ||
+            //       line < 0 || line >= numbers.GetLength(0))
+            //{
+            //    Console.Write("Ошибка! Введите номер строки (0-2): ");
+            //}
 
             //int sum = 0;
-            // //перебераем элементы строки и суммируем их
-            //for (int j = 0; j < 5; j++)
+
+            //// Вычисляем сумму строки
+            //for (int j = 0; j < numbers.GetLength(1); j++)
             //{
             //    sum += numbers[line, j];
             //}
@@ -33,95 +51,122 @@
             //2
             //int[,] numbers = new int[3, 4];
 
-            //Console.WriteLine("таблица с числами");
-            //  //строки 
-            //for (int i = 0; i < 3; i++)
-            //{    //столбики
-            //    for (int j = 0; j < 4; j++)
+            //Console.WriteLine("Двумерный массив:");
+
+            //// создаем таблицу с рандомными числами от 10 до 100
+            //for (int i = 0; i < numbers.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < numbers.GetLength(1); j++)
             //    {
             //        numbers[i, j] = new Random().Next(10, 101);
+            //    }
+            //}
+
+            //// Выводим числа масива в виде таблицы
+            //for (int i = 0; i < numbers.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < numbers.GetLength(1); j++)
+            //    {
             //        Console.Write(numbers[i, j] + "\t");
             //    }
+
             //    Console.WriteLine();
             //}
 
             //int maxSum = 0;
             //int maxColumn = 0;
-            //// перебираем столбцы
-            //for (int j = 0; j < 4; j++)
+            //int[] sums = new int[numbers.GetLength(1)];
+
+            //// Вычисляем сумму каждого столбца
+            //for (int j = 0; j < numbers.GetLength(1); j++)
             //{
             //    int sum = 0;
 
-            //    for (int i = 0; i < 3; i++)
-            //    {   //добавляем в очередь
+            //    for (int i = 0; i < numbers.GetLength(0); i++)
+            //    {
             //        sum += numbers[i, j];
             //    }
 
-            //    Console.WriteLine($"Сумма {j + 1} столбца = {sum}");
+            //    sums[j] = sum;
 
             //    if (j == 0 || sum > maxSum)
             //    {
             //        maxSum = sum;
-            //        maxColumn = j + 1;
+            //        maxColumn = j;
             //    }
             //}
 
-            //Console.WriteLine($"Столбец с максимальной суммой = {maxColumn}");
+            //// Выводим суммы столбцов
+            //for (int j = 0; j < numbers.GetLength(1); j++)
+            //{
+            //    Console.WriteLine($"Сумма {j + 1} столбца = {sums[j]}");
+            //}
+
+            //Console.WriteLine($"Столбец с максимальной суммой = {maxColumn + 1}");
             //Console.WriteLine($"Максимальная сумма = {maxSum}");
-
             //3
-            int[,] numbers = new int[4, 4];
+            //int[,] numbers = new int[4, 4];
 
-            Console.WriteLine("Матрица:");
+            //Console.WriteLine("Матрица");
 
-            for (int i = 0; i < 4; i++)
-            {             //создаем таблицу 4 на 4
-                for (int j = 0; j < 4; j++)
-                {
-                    numbers[i, j] = new Random().Next(-10, 11);
-                    Console.Write(numbers[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
+            //// Заполняем матрицу
+            //for (int i = 0; i < numbers.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < numbers.GetLength(1); j++)
+            //    {
+            //        numbers[i, j] = new Random().Next(-10, 11);
+            //    }
+            //}
 
-            bool mainFound = false;
-            //главная диагональ
-            bool sideFound = false;
-            //побочная диагональ
+            //// Выводим числа масива в виде м
+            //for (int i = 0; i < numbers.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < numbers.GetLength(1); j++)
+            //    {
+            //        Console.Write(numbers[i, j] + "\t");
+            //    }
 
-            Console.WriteLine("Положительные элементы главной диагонали:");
-            //перебираем элементы главной диагонали
-            for (int i = 0; i < 4; i++)
-            {
-                if (numbers[i, i] > 0)
-                {
-                    Console.Write(numbers[i, i] + " ");
-                    mainFound = true;
-                }
-            }
-            // положительные элементы главной диагонали
-            if (mainFound == false)
-            {
-                Console.WriteLine("Положительных элементов нет.");
-            }
+            //    Console.WriteLine();
+            //}
 
-            Console.WriteLine();
+            //bool mainFound = false;
+            //bool sideFound = false;
 
-            Console.WriteLine("Положительные элементы побочной диагонали:");
-            //перебираем элементы побочной диагонали
-            for (int i = 0; i < 4; i++)
-            {
-                if (numbers[i, 3 - i] > 0)
-                {     //число на побочной диогонале 
-                    Console.Write(numbers[i, 3 - i] + " ");
-                    sideFound = true;
-                }
-            }
-            // положительные элементы побочной диагонали
-            if (sideFound == false)
-            {
-                Console.WriteLine("Положительных элементов нет.");
-            }
+            //Console.WriteLine("Положительные элементы главной диагонали:");
+
+            //// Главная диагональ
+            //for (int i = 0; i < numbers.GetLength(0); i++)
+            //{
+            //    if (numbers[i, i] > 0)
+            //    {
+            //        Console.Write(numbers[i, i] + " ");
+            //        mainFound = true;
+            //    }
+            //}
+
+            //if (!mainFound)
+            //{
+            //    Console.WriteLine("Положительных элементов нет.");
+            //}
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("Положительные элементы побочной диагонали:");
+
+            //// Побочная диагональ
+            //for (int i = 0; i < numbers.GetLength(0); i++)
+            //{
+            //    if (numbers[i, numbers.GetLength(1) - 1 - i] > 0)
+            //    {
+            //        Console.Write(numbers[i, numbers.GetLength(1) - 1 - i] + " ");
+            //        sideFound = true;
+            //    }
+            //}
+
+            //if (!sideFound)
+            //{
+            //    Console.WriteLine("Положительных элементов нет.");
+            //}
 
         }
     }
